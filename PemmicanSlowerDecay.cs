6133611e5c20e79b289654555a61b98af3ae06f9
BaltaZar <jancsi@gyori.hu>
2025-10-13 21:42:34 +0200

    Add project files.

---
+++ PemmicanSlowerDecay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MelonLoader;
+using HarmonyLib;
+using Il2Cpp;
+
+public class Main : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        LoggerInstance.Msg("PemmicanSlowerDecay loaded successfully!");
+    }
+}
+
+[HarmonyPatch(typeof(GearItem), nameof(GearItem.Degrade), new Type[] { typeof(float) })]
+internal static class GearItem_Degrade_Pemmican
+{
+    private static void Prefix(GearItem __instance, ref float hp)
+    {
+        if (__instance == null || string.IsNullOrEmpty(__instance.name))
+            return;
+
+        if (!__instance.name.Equals("GEAR_CookedBarPemmican", StringComparison.OrdinalIgnoreCase))
+            return;
+
+        // Same as CuredMeat outdoors
+        float decayMultiplier = 0.0125f;
+
+        hp *= decayMultiplier;
+    }
+}
